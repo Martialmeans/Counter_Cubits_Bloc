f7201295efd1795c6b06b099c9a3e327dc0914f0
Martialmeans <musamaluleke65@gmail.com>
2024-12-18 15:44:46 +0200

    Added shared preferences to store counter

--- lib/cubits/counter/counter_cubit.dart
+++ lib/cubits/counter/counter_cubit.dart
@@ -1,9 +1,38 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
+  CounterCubit() : super(0) {
+    _loadInitialState();
+  }
 
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
-  void restart() => emit(0);
+  Future<void> _loadInitialState() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    int counter = pref.getInt('state') ?? 0;
+    emit(counter);
+  }
+
+  Future<void> increment() async {
+    final newState = state + 1;
+    emit(newState);
+
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    await pref.setInt('state', newState);
+  }
+
+  Future<void> decrement() async {
+    final newState = state - 1;
+    emit(newState);
+
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    await pref.setInt('state', newState);
+  }
+
+  Future<void> restart() async {
+    const newState = 0;
+    emit(0);
+
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    await pref.setInt('state', newState);
+  }
 }
